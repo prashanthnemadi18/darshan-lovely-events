a7f11255332aacedd41904abf570f52ffad78262
Prashanth Nemadi <prashanthnemadi@example.com>
2026-03-03 18:11:44 +0530

    Update CORS settings

--- backend/server.py
+++ backend/server.py
@@ -105,7 +105,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["*"],  # Allow all origins for now, will update after Vercel deployment
     allow_methods=["*"],
     allow_headers=["*"],
 )
